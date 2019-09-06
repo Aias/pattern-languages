f1bbec4f9ad9982350dd6f7bb0ac7770ec7c8500
Nick Trombley <trombley.nick@gmail.com>
2019-09-06 18:37:19 +0900

    Strip out a bunch of shit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,15 @@ require('dotenv').config({
 	path: `.env.${process.env.NODE_ENV}`,
 })
 
+const bases = {
+	patterns: `appfoH6qlLxFCVhV5`,
+}
+
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@gatsbyjs`,
+		title: `patternsof.design`,
+		description: `Each pattern is a rule which describes what you have to do to generate the entity which it defines. It is a three-part rule, which expresses a relation between a certain context, a problem, and a solution.`,
+		author: `Nick Trombley`,
 	},
 	plugins: [
 		`gatsby-plugin-react-helmet`,
@@ -43,9 +47,41 @@ module.exports = {
 				apiKey: process.env.AIRTABLE_API_KEY, // may instead specify via env, see below
 				tables: [
 					{
-						baseId: `appfoH6qlLxFCVhV5`,
+						baseId: bases.patterns,
 						tableName: `languages`,
-						// tableView: `Grid view`,
+						tableView: `Grid view`,
+						tableLinks: ['sections', 'patterns', 'orders'],
+					},
+					{
+						baseId: bases.patterns,
+						tableName: `sections`,
+						tableView: `Grid view`,
+						tableLinks: ['language', 'patterns', 'category'],
+					},
+					{
+						baseId: bases.patterns,
+						tableName: `patterns`,
+						tableView: `Grid view`,
+						tableLinks: [
+							'language',
+							'section',
+							'supports',
+							'refers_to',
+							'depends_on',
+							'orders',
+						],
+					},
+					{
+						baseId: bases.patterns,
+						tableName: `orders`,
+						tableView: `Grid view`,
+						tableLinks: ['language', 'patterns'],
+					},
+					{
+						baseId: bases.patterns,
+						tableName: `sequences`,
+						tableView: `Grid view`,
+						tableLinks: ['patterns'],
 					},
 				],
 			},
